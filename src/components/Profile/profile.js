ec583cc12b10cdb7585ac04f4f81eb9f175bffe2
Anshul Dubey <anshuldubey2166@gmail.com>
2018-12-21 09:26:56 +0530

    Added profile pic

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import CardMedia from "@material-ui/core/CardMedia";
 
 class Profile extends Component {
   render() {
@@ -8,10 +9,18 @@ class Profile extends Component {
       <div className="profile-row">
         <Grid container spacing={24}>
           <Grid item xs={12} sm={6} md={4}>
-            <Paper>Profile Pic</Paper>
+            <CardMedia
+              component="img"
+              alt="Profile Pic"
+              image="/assets/alberto.png"
+              title="Profile Pic"
+              className="image-round image-center"
+              width="150"
+              height="150"
+            />
           </Grid>
           <Grid item xs={12} sm={6} md={8}>
-            <Paper>xs=12 sm=6</Paper>
+            <Paper>User details</Paper>
           </Grid>
         </Grid>
       </div>
