b932e0eef82731de83eea87ac46dcd933ad8c2f4
Anshul Dubey <anshuldubey2166@gmail.com>
2018-12-27 16:01:42 +0530

    Screenshot working

--- src/components/CourseInfo/CourseInfo.js
+++ src/components/CourseInfo/CourseInfo.js
@@ -5,13 +5,14 @@ import CardMedia from "@material-ui/core/CardMedia";
 import StarRatings from "react-star-ratings";
 import ListBox from "../Profile/ListBox/ListBox";
 import "./CourseInfo.css";
-import {data} from "./data";
+import { data } from "./data";
 
-class CourseInfo extends Component {  
+class CourseInfo extends Component {
   render() {
-    const Overview=data.overview.map((paragraph)=>
-        <p>{paragraph}</p>
-    );
+    const Overview = data.overview.map(paragraph => <p>{paragraph}</p>);
+    const Screenshots = data.screenshots.map(screenshot => (
+      <img src={screenshot} className="screenshots" />
+    ));
     return (
       <div className="course-info">
         <Grid container spacing={24} className="top-row-course-info">
@@ -28,9 +29,7 @@ class CourseInfo extends Component {
           </Grid>
           <Grid item md={8} className="vertical-align">
             <h2>
-              <b className="heading-form">
-                {data.course_title}
-              </b>
+              <b className="heading-form">{data.course_title}</b>
             </h2>
             <p>
               <i>Offered by {data.offeredBy}</i>
@@ -46,28 +45,9 @@ class CourseInfo extends Component {
           </Grid>
         </Grid>
         <h1>Overview</h1>
-        {/* <p>
-          CS50 is a true, comprehensive introduction to computer science. The
-          course is taught by the vivacious David Malan and hosted on edX. There
-          are 13 weeks of instruction with 8 mandatory problem sets (psets) and
-          a final project:
-        </p>
-        <p>
-          The CS50 staff say that the pointers section of the course is the
-          hardest and they are right. You intimately deal with pointers and
-          allocating memory in pset5 and pset6. Mental gymnastics are required.
-          Finally figuring out pointers is probably the thing I am most proud of
-          coming out of the course.
-        </p>
-        <p>
-          The 10–20 hours for each problem set is accurate. Your code won’t work
-          and you will get frustrated (especially because half of the course is
-          in C), but frustration is good. Frustration conquered is learning.
-        </p> */}
         {Overview}
         <h1>Screenshots</h1>
-        <img src="/assets/cs501.png" className="screenshots" />
-        <img src="/assets/cs502.png" className="screenshots" />
+        {Screenshots}
         <Grid container spacing={24}>
           <Grid item xs={12} sm={12} md={10} lg={11}>
             <ListBox
