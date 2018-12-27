8f6f382883f8366ffc9feb77fe83843ef6a57c77
Anshul Dubey <anshuldubey2166@gmail.com>
2018-12-27 21:22:05 +0530

    Added course-info

--- src/components/CourseInfo/data.js
+++ src/components/CourseInfo/data.js
@@ -3,7 +3,15 @@ export const data={
     course_title:"An introduction to Computer Science",
     offeredBy:"Coursera",
     rating:4.5,
-    overview:[" CS50 is a true, comprehensive introduction to computer science. The course is taught by the vivacious David Malan and hosted on edX. There are 13 weeks of instruction with 8 mandatory problem sets (psets) and a final project:","The CS50 staff say that the pointers section of the course is the hardest and they are right. You intimately deal with pointers and allocating memory in pset5 and pset6. Mental gymnastics are required. Finally figuring out pointers is probably the thing I am most proud of coming out of the course.","The 10–20 hours for each problem set is accurate. Your code won’t work and you will get frustrated (especially because half of the course is in C), but frustration is good. Frustration conquered is learning."],
+    overview:[" CS50 is a true, comprehensive introduction to computer science. The course is taught by the vivacious David Malan and hosted on edX. There are 13 weeks of instruction with 8 mandatory problem sets (psets) and a final project:",
+    "The CS50 staff say that the pointers section of the course is the hardest and they are right. You intimately deal with pointers and allocating memory in pset5 and pset6. Mental gymnastics are required. Finally figuring out pointers is probably the thing I am most proud of coming out of the course.",
+    "The 10–20 hours for each problem set is accurate. Your code won’t work and you will get frustrated (especially because half of the course is in C), but frustration is good. Frustration conquered is learning."],
     screenshots:['/assets/cs501.png','/assets/cs501.png'],
     topics_covered:["React, NodeJs, Python"]
 };
+
+// overview contains paragrpahs in arrays like ["para1","para2","para3"]
+// Means that the first element is the first para, second element is second para and so on
+
+//For screenshots, use urls
+//Same for course_image
