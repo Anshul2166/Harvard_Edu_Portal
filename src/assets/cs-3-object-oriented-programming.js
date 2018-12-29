bbcf6903117fb9ed71e30e1bbc5d4a661b479c5c
Anshul Dubey <anshuldubey2166@gmail.com>
2018-12-29 18:50:43 +0530

    Added data files

---
+++ src/assets/cs-3-object-oriented-programming.js
@@ -0,0 +1,15 @@
+export const data={
+    course_image:"/assets/cs53.jpg",
+    course_title:"An introduction to object oriented programming",
+    offeredBy:"youtube- nptel - course-ware",
+    url : https://www.youtube.com/playlist?list=PL-AyYXNLDtPZ37yxlhz1CCfBmHlHOfaVb,
+    overview:[" primary goal is for you to learn to appreciate and use the fundamental design principles of modularity and abstraction in a variety of contexts from electrical engineering and computer science. To achieve this goal, we will study electrical engineering (EE) and computer science (CS) largely from the perspective of how to build systems that interact with, and attempt to control, an external environment."],
+    screenshots:['/assets/cs503.png','/assets/cs503.png'],
+    topics_covered:["Procedural Programming Paradigm - PPP • Object-Oriented Programming Paradigm - OPP • Objects •State and Behavior • Software Objects - Fields and Methods • Data Encapsulation • Public Interface • Object-based application • Benefits of object-based application development • Classes • Definition • Blueprint of similar object • Instantiating objects from class - example • Object-Oriented Principles •Encapsulation •Inheritance and Interfaces •Polymorphism"]
+};
+
+// overview contains paragrpahs in arrays like ["para1","para2","para3"]
+// Means that the first element is the first para, second element is second para and so on
+
+//For screenshots, use urls
+//Same for course_image
