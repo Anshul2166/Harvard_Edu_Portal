21db9bf37528c1af928f9f5ffc603839eef7dd45
samratluitel <ngc2058@gmail.com>
2019-02-06 00:24:09 +0545

    Single post card finished

--- src/components/Common/PostCard/PostCard.js
+++ src/components/Common/PostCard/PostCard.js
@@ -5,9 +5,65 @@ class PostCard extends Component {
     return (
       <div className="PostCard">
         <div className="PostCard__side-action">
-          <i className="fas fa-arrow-up" />
+          <div className="PostCard__upvote-wrapper">
+            <div className="PostCard__upvote PostCard__side-action-btn">
+              <i className="fas fa-arrow-up" />
+            </div>
+            <div className="PostCard__vote-amount">2</div>
+            <div className="PostCard__downvote PostCard__side-action-btn">
+              <i className="fas fa-arrow-down" />
+            </div>
+          </div>
         </div>
-        <div className="PostCard__main-area">Main Area goes here</div>
+        <div className="PostCard__main-area">
+          <div className="PostCard__main-area__post-info-wrapper">
+            <div className="PostCard__main-area__post-info">
+              <div className="PostCard__community-wrapper">
+                <div className="PostCard__community-name">
+                  r/Computer Enginering
+                </div>
+                <div className="PostCard__author-name">
+                  Posted by Samrat Luintel
+                </div>
+              </div>
+              <div className="PostCard__post-title">
+                Captain America was a spy of Hydra
+              </div>
+              <div className="PostCard__post-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+                voluptate, qui molestias assumenda praesentium ratione error
+                tempore id tenetur illum, laudantium explicabo ipsa a minima
+                blanditiis quasi odio officiis nostrum odit, fugit officia
+                consequuntur. Sunt similique qui, facilis eos sequi fuga debitis
+                tenetur modi nisi quasi ut dolorum ullam tempora unde
+                repudiandae culpa sint ea temporibus ipsa velit voluptatum atque
+                aperiam aliquid totam? Id quis, magnam corrupti aperiam
+                voluptatem ipsa suscipit sed nobis a reprehenderit dolore
+                quaerat placeat velit veritatis. Mollitia quod sapiente incidunt
+                eligendi nulla placeat. Eum quas, voluptates, rerum rem
+                doloribus harum vero cum, inventore accusamus amet explicabo!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Assumenda nesciunt vel, reprehenderit cumque officiis voluptatem
+                minus nostrum. Placeat totam voluptatum aperiam, adipisci amet
+                qui deleniti in iure, praesentium voluptates rerum, sint non
+                distinctio odit ullam labore voluptatibus excepturi a eligendi
+                fugiat natus! Illo officia, neque itaque veniam soluta fugiat
+                dolore quibusdam quaerat incidunt eligendi beatae amet ea autem
+                molestiae id nihil consequuntur cum exercitationem molestias
+                unde a ipsam excepturi. Facere maiores voluptatibus ratione unde
+                saepe? Blanditiis adipisci ut magnam tenetur. Aut fugit
+                architecto, assumenda ad dignissimos eos voluptate alias
+                reiciendis, cupiditate aliquid aperiam temporibus laboriosam
+                tempora eius. Cumque, eligendi culpa?
+              </div>
+            </div>
+            <div className="PostCard__post-additional-info">
+              <div className="PostCard__comments PostCard__post-additional-info__single-item">
+                <i className="fas fa-comments" /> 5 Comments
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
