be97c1e9592e9c4f0ba1c3f7ed287c8fafe72599
samratluitel <ngc2058@gmail.com>
2019-02-12 19:32:36 +0545

    Landing Section made responsive

--- src/components/Landing/LandingHeader/LandingHeader.js
+++ src/components/Landing/LandingHeader/LandingHeader.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 import Logo from "../../../assets/images/logo.png";
+import { Link } from "react-scroll";
+import classnames from "classnames";
+
 class LandingHeader extends Component {
+  state = {
+    mobileNavOpen: false
+  };
+
+  toggleMobileNav = () => {
+    this.setState(prevState => ({
+      mobileNavOpen: !prevState.mobileNavOpen
+    }));
+  };
+
   render() {
     return (
       <div className="LandingHeader">
@@ -16,15 +29,67 @@ class LandingHeader extends Component {
                 Learn<span>It</span>
               </div>
             </div>
-
-            <div className="LandingHeader__nav-wrapper">
-              <div className="LandingHeader__nav-item">Home</div>
-              <div className="LandingHeader__nav-item">About</div>
-              <div className="LandingHeader__nav-item">Service</div>
-              <div className="LandingHeader__nav-item">Contact Us</div>
-              <div className="LandingHeader__nav-item">Home</div>
-              <div className="LandingHeader__nav-item">
-                <div className="LandingHeader__action-btn">Get Started</div>
+            <div className="LandingHeader__nav-parent-wrapper">
+              <div
+                className="LandingHeader__toggle-mobile-nav-btn"
+                onClick={this.toggleMobileNav}
+              >
+                <i className="fas fa-bars" />
+              </div>
+              <div
+                className={classnames({
+                  "LandingHeader__nav-wrapper": true,
+                  "LandingHeader__nav-wrapper--close ": !this.state
+                    .mobileNavOpen
+                })}
+              >
+                <Link
+                  className="LandingHeader__nav-item"
+                  activeClass="LandingHeader__nav-item--selected"
+                  to="LandingHero"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  Home
+                </Link>
+                <Link
+                  className="LandingHeader__nav-item"
+                  activeClass="LandingHeader__nav-item--selected"
+                  to="LandingWhyUs"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  About
+                </Link>
+                <Link
+                  className="LandingHeader__nav-item"
+                  activeClass="LandingHeader__nav-item--selected"
+                  to="LandingAdditionalInfo"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  Why Us
+                </Link>
+                <Link
+                  className="LandingHeader__nav-item"
+                  activeClass="LandingHeader__nav-item--selected"
+                  to="User"
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  Join
+                </Link>
+                <div className="LandingHeader__nav-item">
+                  <div className="LandingHeader__action-btn">Get Started</div>
+                </div>
               </div>
             </div>
           </div>
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -41,87 +41,95 @@ class User extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <Grid container spacing={24} className="user">
-        <Grid item md={1} />
-        <Grid item md={5} className="vertical-center">
-          <Typography
-            variant="subheading"
-            component="p"
-            className="form-center"
-          >
-            <ul>
-              <li>
-                <CheckIcon />
-                Learn from our carefully prepared course path crafted by our
-                expert
-              </li>
-              <li>
-                <CheckIcon />
-                Explore oppurtunities in your field in India and abroad
-              </li>
-              <li>
-                <CheckIcon />
-                Get to know opinions of others on your area of interest
-              </li>
-              <li>
-                <CheckIcon />
-                Get information on best resources available out there to optimize your learning
-              </li>
-              <li>
-                <CheckIcon />
-                Learn smart by devoting time on learning skills which are important
-              </li>
-            </ul>
-          </Typography>
-        </Grid>
-        <Grid item md={6}>
-          <div className="border-box">
+      <div className="User">
+        <Grid container spacing={24} className="user">
+          <Grid item md={1} />
+          <Grid item md={5} className="vertical-center">
             <Typography
-              variant="display1"
-              component="h1"
-              className="form-center center-text"
+              variant="subheading"
+              component="p"
+              className="form-center"
             >
-              <b className="heading-form">Get started with learnIt</b>
+              <ul>
+                <li>
+                  <CheckIcon />
+                  Learn from our carefully prepared course path crafted by our
+                  expert
+                </li>
+                <li>
+                  <CheckIcon />
+                  Explore oppurtunities in your field in India and abroad
+                </li>
+                <li>
+                  <CheckIcon />
+                  Get to know opinions of others on your area of interest
+                </li>
+                <li>
+                  <CheckIcon />
+                  Get information on best resources available out there to
+                  optimize your learning
+                </li>
+                <li>
+                  <CheckIcon />
+                  Learn smart by devoting time on learning skills which are
+                  important
+                </li>
+              </ul>
             </Typography>
-            <div className="center-div-user">
-              <form className={classes.container} noValidate autoComplete="off">
-                <FormGroup className="form-center">
-                  <FormControl>
-                    <TextField
-                      required
-                      id="standard-required"
-                      label="Username"
-                      placeholder="Hello World"
-                      margin="normal"
-                      className={classes.textField + " textfield"}
-                    />
-                  </FormControl>
-                  <FormControl>
-                    <TextField
-                      required
-                      id="standard-password-input"
-                      label="Password"
-                      className={classes.textField + " textfield"}
-                      type="password"
-                      autoComplete="current-password"
-                      margin="small"
-                    />
-                  </FormControl>
-                </FormGroup>
-              </form>
-              <Button
-                variant="contained"
-                color="primary"
-                className={classes.button}
+          </Grid>
+          <Grid item md={6}>
+            <div className="border-box">
+              <Typography
+                variant="display1"
+                component="h1"
+                className="form-center center-text"
               >
-                Login with username
-              </Button>
-              <Divider />
-              <SocialNetworkLogIn classes={classes} />
+                <b className="heading-form">Get started with learnIt</b>
+              </Typography>
+              <div className="center-div-user">
+                <form
+                  className={classes.container}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <FormGroup className="form-center">
+                    <FormControl>
+                      <TextField
+                        required
+                        id="standard-required"
+                        label="Username"
+                        placeholder="Hello World"
+                        margin="normal"
+                        className={classes.textField + " textfield"}
+                      />
+                    </FormControl>
+                    <FormControl>
+                      <TextField
+                        required
+                        id="standard-password-input"
+                        label="Password"
+                        className={classes.textField + " textfield"}
+                        type="password"
+                        autoComplete="current-password"
+                        margin="small"
+                      />
+                    </FormControl>
+                  </FormGroup>
+                </form>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.button}
+                >
+                  Login with username
+                </Button>
+                <Divider />
+                <SocialNetworkLogIn classes={classes} />
+              </div>
             </div>
-          </div>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     );
   }
 }
@@ -137,14 +145,14 @@ const SocialNetworkLogIn = props => (
       Login with facebook
     </Button>
     <a href="http://localhost:8080/api/users/auth/google">
-    <Button
-      variant="contained"
-      color="primary"
-      className={props.classes.button}
-    >
-      <GoogleIcon />
-      Login with google
-    </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        className={props.classes.button}
+      >
+        <GoogleIcon />
+        Login with google
+      </Button>
     </a>
   </div>
 );
