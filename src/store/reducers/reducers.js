5e850915ff0763cb28e63d248ca309afdc89b67e
Anshul Dubey <anshuldubey2166@gmail.com>
2019-02-14 21:05:45 +0530

    Fixed reducer issue

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -2,9 +2,13 @@ import { combineReducers } from "redux";
 import courseReducer from "./courseReducer";
 import communitiesReducer from "./communitiesReducer";
 import postsReducer from "./postsReducer";
+import specializationReducer from "./specializationReducer";
+import degreeReducer from "./degreeReducer";
 
 export default combineReducers({
   courses: courseReducer,
   communities: communitiesReducer,
-  posts: postsReducer
+  posts: postsReducer,
+  specialization:specializationReducer,
+  degrees:degreeReducer
 });
