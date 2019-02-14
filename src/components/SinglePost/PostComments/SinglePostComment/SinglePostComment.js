a927df6bee2a42bce47fdc2e74c5c4069a7d4abb
samratluitel <ngc2058@gmail.com>
2019-02-14 19:26:26 +0545

    Comment sync with backend

--- src/components/SinglePost/PostComments/SinglePostComment/SinglePostComment.js
+++ src/components/SinglePost/PostComments/SinglePostComment/SinglePostComment.js
@@ -16,7 +16,9 @@ class SinglePostComment extends Component {
         <div className="SinglePostComment__content-wrapper">
           <div className="SinglePostComment__header">
             {/* Display author name here */}
-            <div className="SinglePostComment__author-name">{props.author}</div>
+            <div className="SinglePostComment__author-name">
+              {props.author.local.username}
+            </div>
             <div className="SinglePostComment__posted-time">9 hours ago</div>
           </div>
           <div className="SinglePostComment__text">{props.content}</div>
