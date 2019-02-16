ee591f895b79a26740849e4bcbd2232031de4552
Anshul Dubey <anshuldubey2166@gmail.com>
2019-02-16 21:56:21 +0530

    Checkbox strike working

--- src/components/Common/Modal/ProfileInfoModal.js
+++ src/components/Common/Modal/ProfileInfoModal.js
@@ -5,11 +5,10 @@ import Typography from "@material-ui/core/Typography";
 import Modal from "@material-ui/core/Modal";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
-import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
-import Grid from "@material-ui/core/Grid";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 function getModalStyle() {
   const top = 50;
@@ -29,24 +28,65 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 4,
-    outline: "none"
+    outline: "none",
+    paddingTop: 0
   },
   title: {
-    margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`,
-    paddingLeft:16
+    margin: `${theme.spacing.unit * 4}px 0 ${theme.spacing.unit * 2}px`
   },
+  listItem: {
+    padding: 0
+  }
 });
 
 class ProfileModal extends React.Component {
+  state = {
+    bufferText: "",
+    bufferChecked: Array(this.props.data.length).fill(true)
+  };
+  componentWillReceiveProps(newProps) {
+    this.setState({ bufferChecked: Array(newProps.data.length).fill(true) });
+    this.bufferRecords = newProps.data;
+  }
+  onAlterCheckbox = (event, index) => {
+    let bufferChecked=this.state.bufferChecked.slice();
+    if (event.target.checked) {
+      bufferChecked[index] = true;
+    } else {
+      bufferChecked[index] = false;
+    }
+    this.setState({ bufferChecked: bufferChecked });
+  };
+  onClickAdd = event => {
+    if (event.key === "Enter") {
+      this.bufferRecords.push(this.state.bufferText);
+      let bufferChecked=this.state.bufferChecked.slice();
+      bufferChecked.push(true);
+      this.setState({ bufferChecked: bufferChecked });
+    }
+  };
+  changeBufferText = event => {
+    this.setState({ bufferText: event.target.text });
+  };
   render() {
     const { classes, data } = this.props;
-    function generate(element) {
-      return data.map(value =>
-        React.cloneElement(element, {
-          key: value
-        })
-      );
-    }
+    const list = data.map((item, index) => (
+      <ListItem divider className={classes.listItem} key={index}>
+        <Checkbox
+          value="checkedC"
+          onClick={event => this.onAlterCheckbox(event, index)}
+          checked={this.state.bufferChecked[index]}
+        />
+        <ListItemText
+          primary={item}
+          style={
+            this.state.bufferChecked[index]
+              ? { textDecoration: "" }
+              : { textDecoration: "line-through" }
+          }
+        />
+      </ListItem>
+    ));
     return (
       <Modal
         aria-labelledby="simple-modal-title"
@@ -56,23 +96,27 @@ class ProfileModal extends React.Component {
       >
         <div style={getModalStyle()} className={classes.paper}>
           <Typography variant="h6" className={classes.title}>
-            Icon with text
+            Add or delete
           </Typography>
           <div className={classes.demo}>
-            <List dense={false}>
-              {generate(
-                <ListItem>
-                  <ListItemText primary="Single-line item" />
-                  <ListItemSecondaryAction>
-                    <IconButton aria-label="Delete">
-                      <DeleteIcon />
-                    </IconButton>
-                  </ListItemSecondaryAction>
-                </ListItem>
-              )}
+            <List dense={false} disablePadding>
+              {list}{" "}
+              <span className={classes.textField}>
+                <TextField
+                  id="standard-with-placeholder"
+                  label="Add here"
+                  placeholder="Placeholder"
+                  margin="normal"
+                />
+              </span>
             </List>
           </div>
-          <ProfileInfoModal />
+
+          <div className={classes.textCenter}>
+            <Button variant="contained" color="primary">
+              Save
+            </Button>
+          </div>
         </div>
       </Modal>
     );
