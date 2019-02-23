04a6556c6dca14bc8a389a758a9cb93ca2e3be24
samratluitel <ngc2058@gmail.com>
2019-02-23 23:11:44 +0545

    Preparing file for deployment

--- server/app.js
+++ server/app.js
@@ -1,13 +1,23 @@
 const app = require("./server");
 const http = require("http");
 const server = http.createServer(app);
-
-
+const path = require("path");
+const express = require("express");
 // Set Variable called port according to "development" / "production"
 if (process.env.NODE_ENV === "production") {
   app.set("port", process.env.PORT || 80);
 } else {
   app.set("port", 8080);
+}
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // if not https redirect to https unless logging in using OAuth
+
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.get("*", (req, res) => {
+    console.log("fall back route is being called");
+    res.sendFile(path.join(__dirname + "/../build/index.html"));
+  });
 }
 
 server.listen(app.get("port"), () => {
