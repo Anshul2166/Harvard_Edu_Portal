7e8c1e363b96f4ea800a5f5f8831e8f3aeff16cf
Anshul Dubey <anshuldubey2166@gmail.com>
2019-06-04 01:03:46 +0530

    Navbar title link working

--- src/components/Common/NavHeader/NavHeader.js
+++ src/components/Common/NavHeader/NavHeader.js
@@ -1,247 +1,240 @@
-import React from "react";
-import PropTypes from "prop-types";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-import { fade } from "@material-ui/core/styles/colorManipulator";
-import { withStyles } from "@material-ui/core/styles";
-import MenuIcon from "@material-ui/icons/Menu";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MoreIcon from "@material-ui/icons/MoreVert";
-import Button from "@material-ui/core/Button";
-import {Redirect,Link,withRouter} from "react-router-dom";
-import axios from "axios";
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
+import { Redirect, Link, withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 const styles = theme => ({
-  root: {
-    width: "100%"
-  },
-  grow: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing.unit * 2,
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing.unit * 3,
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit",
-    width: "100%"
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: 200
-    }
-  },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex"
-    }
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none"
-    }
-  },
-  navButton: {
-    marginLeft: 25,
-    marginRight: 25,
-    color:"white!important"
-  },
-  accountCircle:{
-      color:"white"
-  },
-  links:{
-    textDecoration:"none"
-  }
+	root: {
+		width: '100%',
+	},
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	},
+	title: {
+		display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			display: 'block',
+		},
+	},
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginRight: theme.spacing.unit * 2,
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing.unit * 3,
+			width: 'auto',
+		},
+	},
+	searchIcon: {
+		width: theme.spacing.unit * 9,
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	inputRoot: {
+		color: 'inherit',
+		width: '100%',
+	},
+	inputInput: {
+		paddingTop: theme.spacing.unit,
+		paddingRight: theme.spacing.unit,
+		paddingBottom: theme.spacing.unit,
+		paddingLeft: theme.spacing.unit * 10,
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: 200,
+		},
+	},
+	sectionDesktop: {
+		display: 'none',
+		[theme.breakpoints.up('md')]: {
+			display: 'flex',
+		},
+	},
+	sectionMobile: {
+		display: 'flex',
+		[theme.breakpoints.up('md')]: {
+			display: 'none',
+		},
+	},
+	navButton: {
+		marginLeft: 25,
+		marginRight: 25,
+		color: 'white!important',
+	},
+	accountCircle: {
+		color: 'white',
+	},
+	links: {
+		textDecoration: 'none',
+	},
 });
 
 class PrimarySearchAppBar extends React.Component {
-  state = {
-    anchorEl: null,
-    mobileMoreAnchorEl: null
-  };
+	state = {
+		anchorEl: null,
+		mobileMoreAnchorEl: null,
+	};
 
-  handleProfileMenuOpen = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
+	handleProfileMenuOpen = event => {
+		this.setState({ anchorEl: event.currentTarget });
+	};
 
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-    this.handleMobileMenuClose();
-  };
+	handleMenuClose = () => {
+		this.setState({ anchorEl: null });
+		this.handleMobileMenuClose();
+	};
 
-  handleMobileMenuOpen = event => {
-    this.setState({ mobileMoreAnchorEl: event.currentTarget });
-  };
+	handleMobileMenuOpen = event => {
+		this.setState({ mobileMoreAnchorEl: event.currentTarget });
+	};
 
-  handleMobileMenuClose = () => {
-    this.setState({ mobileMoreAnchorEl: null });
-  };
+	handleMobileMenuClose = () => {
+		this.setState({ mobileMoreAnchorEl: null });
+	};
 
-  clickLogout=async ()=>{
-    try{
-    const response=await axios.get("/api/users/logout");
-    window.location.href="/";
-    }catch(err){
-      console.log(err);
-    }
-  }
+	clickLogout = async () => {
+		try {
+			const response = await axios.get('/api/users/logout');
+			window.location.href = '/';
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  render() {
-    const { anchorEl, mobileMoreAnchorEl } = this.state;
-    const { classes } = this.props;
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);    
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        open={isMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <Link className={classes.links} to="/profile"><MenuItem onClick={this.handleMenuClose}>Profile</MenuItem></Link>
-        <span onClick={this.clickLogout}><MenuItem onClick={this.handleMenuClose}>Logout</MenuItem></span>
-      </Menu>
-    );
+	render() {
+		const { anchorEl, mobileMoreAnchorEl } = this.state;
+		const { classes } = this.props;
+		const isMenuOpen = Boolean(anchorEl);
+		const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+		const renderMenu = (
+			<Menu
+				anchorEl={anchorEl}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+				transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+				open={isMenuOpen}
+				onClose={this.handleMenuClose}
+			>
+				<Link className={classes.links} to="/profile">
+					<MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
+				</Link>
+				<span onClick={this.clickLogout}>
+					<MenuItem onClick={this.handleMenuClose}>Logout</MenuItem>
+				</span>
+			</Menu>
+		);
 
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        open={isMobileMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <Button color="inherit">Sunburst</Button>
-        </MenuItem>
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <Button color="inherit">Dashboard</Button>
-        </MenuItem>
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <Button color="inherit">Roadmaps</Button>
-        </MenuItem>
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <Button color="inherit">Forums</Button>
-        </MenuItem>
-        <MenuItem onClick={this.handleMobileMenuClose}>
-          <Button color="inherit">Profile</Button>
-        </MenuItem>
-      </Menu>
-    );
+		const renderMobileMenu = (
+			<Menu
+				anchorEl={mobileMoreAnchorEl}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+				transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+				open={isMobileMenuOpen}
+				onClose={this.handleMenuClose}
+			>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<Button color="inherit">Sunburst</Button>
+				</MenuItem>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<Button color="inherit">Dashboard</Button>
+				</MenuItem>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<Button color="inherit">Roadmaps</Button>
+				</MenuItem>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<Button color="inherit">Forums</Button>
+				</MenuItem>
+				<MenuItem onClick={this.handleMobileMenuClose}>
+					<Button color="inherit">Profile</Button>
+				</MenuItem>
+			</Menu>
+		);
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Open drawer"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              className={classes.title}
-              variant="h6"
-              color="inherit"
-              noWrap
-            >
-              Learn It
-            </Typography>
-            <div className={classes.grow} />
-            <div className={classes.sectionDesktop}>
-              <Button color="inherit" className={classes.navButton} href="/sunburst">
-                Sunburst
-              </Button>
-              <Button color="inherit" className={classes.navButton} href="/dashboard" >
-                Dashboard
-              </Button>
-              <Button color="inherit" className={classes.navButton} href="/select-domain">
-                Roadmaps
-              </Button>
-              <Button color="inherit" className={classes.navButton} href="/forum">
-                Forums
-              </Button>
-              <Button color="inherit" className={classes.navButton} href="/select-questions">
-                Practise sets
-              </Button>
-              <Button color="inherit" className={classes.navButton} href="/graph">
-                Graph Path
-              </Button>
-              {/* <Link to="/profile"> */}
-                <IconButton
-                  aria-owns={isMenuOpen ? "material-appbar" : undefined}
-                  aria-haspopup="true"
-                  onClick={this.handleProfileMenuOpen}
-                  className={classes.accountCircle}
-                >
-                  <AccountCircle />
-                </IconButton>
-              {/* </Link> */}
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton
-                aria-haspopup="true"
-                onClick={this.handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MoreIcon />
-              </IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-        {renderMenu}
-        {renderMobileMenu}
-      </div>
-    );
-  }
+		return (
+			<div className={classes.root}>
+				<AppBar position="static">
+					<Toolbar>
+						<IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+							<MenuIcon />
+						</IconButton>
+						<Link to="/dashboard" style={{ textDecoration: 'none',color:'white' }}>
+							<Typography className={classes.title} variant="h6" color="inherit" noWrap>
+								Learn It
+							</Typography>
+						</Link>
+						<div className={classes.grow} />
+						<div className={classes.sectionDesktop}>
+							<Button color="inherit" className={classes.navButton} href="/sunburst">
+								Sunburst
+							</Button>
+							<Button color="inherit" className={classes.navButton} href="/dashboard">
+								Dashboard
+							</Button>
+							<Button color="inherit" className={classes.navButton} href="/select-domain">
+								Roadmaps
+							</Button>
+							<Button color="inherit" className={classes.navButton} href="/forum">
+								Forums
+							</Button>
+							<Button color="inherit" className={classes.navButton} href="/select-questions">
+								Practise sets
+							</Button>
+							<Button color="inherit" className={classes.navButton} href="/graph">
+								Graph Path
+							</Button>
+							{/* <Link to="/profile"> */}
+							<IconButton
+								aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+								aria-haspopup="true"
+								onClick={this.handleProfileMenuOpen}
+								className={classes.accountCircle}
+							>
+								<AccountCircle />
+							</IconButton>
+							{/* </Link> */}
+						</div>
+						<div className={classes.sectionMobile}>
+							<IconButton aria-haspopup="true" onClick={this.handleMobileMenuOpen} color="inherit">
+								<MoreIcon />
+							</IconButton>
+						</div>
+					</Toolbar>
+				</AppBar>
+				{renderMenu}
+				{renderMobileMenu}
+			</div>
+		);
+	}
 }
 
 PrimarySearchAppBar.propTypes = {
-  classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired,
 };
 
 export default withRouter(withStyles(styles)(PrimarySearchAppBar));
